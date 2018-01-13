2eeef5fc5e0eb37cbf72b924975d33765e5e8091
Laurie Gloge <gloge@haathi-consulting.com>
2018-01-12 20:44:58 -0700

    score is visible

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ GRAVITY = Vec[0, 600] #pixels/s^2
 JUMP_VEL = Vec[0, -300] #pixels/s  - is up
 OBSTACLE_SPEED = 200 #pixels/s, was pixels per frame
 OBSTACLE_SPAWN_INTERVAL = 1.3 #seconds
-OBSTACLE_GAP = 200 #pixels
+OBSTACLE_GAP = 400 #pixels
 DEATH_VELOCITY = Vec[50, -500] #pixels/s
 DEATH_ROTATIONAL_VEL =  360 # degrees/s
 
@@ -21,26 +21,27 @@ Rect = DefStruct.new{{
 		def max_y; pos.y + size.y; end
 	end
 
-# replace with vectors
-# Obstacle = DefStruct.new{{
-# 	y: 0,
-# 	x: 0
-# 	}}
+Obstacle = DefStruct.new{{
+	pos: Vec[0, 0],
+	player_has_crossed: false
 
+	}}
 GameState = DefStruct.new{{
+	score: 0,
 	started: false,
 	alive: true, 
 	scroll_x: 0,
 	player_pos: Vec[20, 350],
 	player_vel: Vec[0,0],
 	player_rotation: 0,
-	obstacles: [], # array of Vec
+	obstacles: [], # array of Obstacles
 	obstacle_countdown: OBSTACLE_SPAWN_INTERVAL,
 	}}
 
 class GameWindow < Gosu::Window
 	def initialize(*args)
 		super
+		@font = Gosu::Font.new(self, Gosu.default_font_name, 25)
 		@images = {
 			background: Gosu::Image.new(self, 'images/Fall-Nature-Background-Pictures.jpg', false),
 			foreground: Gosu::Image.new(self, 'images/foreground.png', true),
@@ -74,12 +75,19 @@ class GameWindow < Gosu::Window
 
 		@state.obstacle_countdown -= dt
 		if @state.obstacle_countdown <= 0 
-			@state.obstacles << Vec[width, rand(50..350)]
+			@state.obstacles << Obstacle.new(pos: Vec[width, rand(50..350)])
 			@state.obstacle_countdown += OBSTACLE_SPAWN_INTERVAL
+			# puts @state.obstacles.size
 		end
 		@state.obstacles.each do |obst|
-			obst.x -= dt*OBSTACLE_SPEED
+			obst.pos.x -= dt*OBSTACLE_SPEED
+			if obst.pos.x < @state.player_pos.x && !obst.player_has_crossed && @state.alive
+				@state.score += 1
+				obst.player_has_crossed = true
+			end
 		end
+
+		@state.obstacles.reject! { |obst| obst.pos.x < -@images[:obstacle].width }
 
 		if @state.alive && player_is_colliding? 
 			@state.alive = false
@@ -115,16 +123,17 @@ class GameWindow < Gosu::Window
 		@state.obstacles.each do |obst|
 			img_y = @images[:obstacle].height
 			#top obstacles
-			@images[:obstacle].draw(obst.x, obst.y - img_y, 0)
+			@images[:obstacle].draw(obst.pos.x, obst.pos.y - img_y, 0)
 			#bottom obstacles
 			scale(1, -1) do
-				@images[:obstacle].draw(obst.x, - height - img_y + (height - obst.y - OBSTACLE_GAP), 0)
+				@images[:obstacle].draw(obst.pos.x, - height - img_y + (height - obst.pos.y - OBSTACLE_GAP), 0)
 			end
 		end
 		@images[:player].draw_rot(
 			@state.player_pos.x, @state.player_pos.y, 
 			0, @state.player_rotation,
 			0, 0)
+		@font.draw_rel(@state.score.to_s, width/2.0, 60, 0, 0.5, 0.5)
 
 		#draws the collison boxes
 		# debug_draw
@@ -142,8 +151,8 @@ class GameWindow < Gosu::Window
 		obst_size = Vec[@images[:obstacle].width, @images[:obstacle].height]
 
 		@state.obstacles.flat_map do |obst|
-			top = Rect.new(pos: Vec[obst.x, obst.y - img_y], size: obst_size)
-			bottom = Rect.new( pos: Vec[obst.x, obst.y + OBSTACLE_GAP], size: obst_size)
+			top = Rect.new(pos: Vec[obst.pos.x, obst.pos.y - img_y], size: obst_size)
+			bottom = Rect.new( pos: Vec[obst.pos.x, obst.pos.y + OBSTACLE_GAP], size: obst_size)
 			[top, bottom]
 		end
 	end
